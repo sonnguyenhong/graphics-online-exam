fbc20e0b2d172ad2de76b6f5572a4f3cb8c6d17d
sonnguyenhong <sonnguyenhong291@gmail.com>
2023-08-07 22:00:08 +0700

    fix backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import Header from './components/Header';
-import Question from './components/Question';
-import React, { useEffect, useState } from "react";
-import instance from './configs/axios';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Result from './page/Result';
 import Questions from './page/Questions';
--- frontend/src/configs/axios.js
+++ frontend/src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001/api/v1',
+    baseURL: 'https://graphics-online-test.onrender.com/api/v1',
     headers: {
         'Content-Type': 'application/json',
     }
